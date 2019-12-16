cc28f9e86caaf01ab14251b04565122a5613ba37
CharlotteLbt <charlotte_ls@hotmail.fr>
2019-12-16 16:58:11 +0100

    testCarto

---
+++ global.R
@@ -0,0 +1,5 @@
+library(shiny)
+library(leaflet)
+
+r_colors <- rgb(t(col2rgb(colors()) / 255))
+names(r_colors) <- colors()
--- server.R
+++ server.R
@@ -10,17 +10,8 @@
 library(shiny)
 
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
 
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
-    })
+    source("source/testcarto_server.R", local=TRUE,encoding="UTF-8")$value
 
 })
