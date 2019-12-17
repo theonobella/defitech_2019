864dec966bb05af864892a3bd74fffe74e15c05e
Estelle <estelle.millet@supagro.fr>
2019-12-17 16:46:14 +0100

    Ajout légende à la carte

--- source/carte_server.R
+++ source/carte_server.R
@@ -1,14 +1,14 @@
 
 
+
 # Représentation de l'onde 410nm pour tous les capteurs
 # pal <- colorNumeric(palette = "Reds",domain = Donnees_a_afficher[input$idSelect_indicateur])
-# 
-# m <- 
-# 
+#
+# m <-
+#
 # m
 
 output$map_indic <- renderLeaflet({
-  
   # browser()
   
   # extraction des données
@@ -18,20 +18,33 @@ output$map_indic <- renderLeaflet({
   Donnees_a_afficher = subset(
     Donnees,
     Date == input$idSelect_date,
-    select = c('ID', 'CoordX','CoordY', input$idSelect_indicateur))
+    select = c('ID', 'CoordX', 'CoordY', input$idSelect_indicateur)
+  )
   
   
   
   # calcul de la carte
   pal <- colorNumeric(palette = "Reds",
-                      domain = Donnees[,input$idSelect_indicateur])
+                      domain = Donnees[, input$idSelect_indicateur])
   
   leaflet(Donnees_a_afficher) %>%
-  addTiles() %>%
-  addCircleMarkers(~CoordX, ~CoordY, popup = ~as.character(Donnees_a_afficher[,input$idSelect_indicateur]), label = ~as.character(Donnees_a_afficher[,input$idSelect_indicateur]),
-                   #radius = ~ifelse(type == "ship", 6, 10),
-                   color = ~pal(Donnees_a_afficher[,input$idSelect_indicateur]),
-                   stroke = FALSE, fillOpacity = 0.5
-  )
+    addTiles() %>%
+    addLegend(
+      title = input$idSelect_indicateur,
+      values = Donnees[, input$idSelect_indicateur],
+      pal = pal,
+      opacity = 0.9,
+      position = "bottomright"
+    ) %>%
+    addCircleMarkers(
+      ~ CoordX,
+      ~ CoordY,
+      popup = ~ as.character(Donnees_a_afficher[, input$idSelect_indicateur]),
+      label = ~ as.character(Donnees_a_afficher[, input$idSelect_indicateur]),
+      #radius = ~ifelse(type == "ship", 6, 10),
+      color = ~ pal(Donnees_a_afficher[, input$idSelect_indicateur]),
+      stroke = FALSE,
+      fillOpacity = 0.5
+    )
   
 })
