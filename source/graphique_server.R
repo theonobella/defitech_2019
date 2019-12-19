a6e2286fffe144c9e5c0dd61ec61b3156e6fea4f
Pierre-Alban <pierrra@hotmail.fr>
2019-12-19 10:41:12 +0100

    Correction var actives

--- source/graphique_server.R
+++ source/graphique_server.R
@@ -1,5 +1,26 @@
 
+# Gnerate selcet input
+output$ISIG <- renderUI({
+  # browser()
+  
+  dta <- Donnees()
+  
+  selectInput(inputId = "idSelect_indicateurgraph", 
+              label = "Select among the list: ", 
+              choices = names(dta[5:ncol(Donnees)]))
+
+  dateRangeInput(inputId = "idSelect_daterange", 
+                 label = "Période d'intérêt :", 
+                 start= "01/01/2018",
+                 end=as.Date(Sys.Date(),format=("%d/%m/%Y")),
+                 format="mm/dd/yyyy",
+                 separator="to")
+})
+
+
 output$plot <- renderPlotly({
+  
+  dta <- Donnees()
   
   selectedLines <- dta$Date >= input$idSelect_daterange[1] & dta$Date <= input$idSelect_daterange[2] 
   selectedCols <-  c("Date", input$idSelect_indicateurgraph) 
--- source/graphique_ui.R
+++ source/graphique_ui.R
@@ -1,18 +1,11 @@
+browser()
 sidebarLayout(
   sidebarPanel(
-    selectInput(inputId = "idSelect_indicateurgraph", 
-                label = "Select among the list: ", 
-                choices = names(dta[5:ncol(Donnees)])),
     
-    dateRangeInput(inputId = "idSelect_daterange", 
-                   label = "Période d'intérêt :", 
-                   start= "01/01/2018",
-                   end=as.Date(Sys.Date(),format=("%d/%m/%Y")),
-                   format="mm/dd/yyyy",
-                   separator="to")
+    uiOutput("ISIG"),
+  
   ),
   mainPanel(
     plotlyOutput("plot")
   )
-  
 )
